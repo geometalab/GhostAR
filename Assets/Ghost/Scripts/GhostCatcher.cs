ef65a6ebaaedc0f7ea16f507379fc372b8c30a49
Lucas <lucaskvn@gmail.com>
2018-04-24 15:47:53 +0200

    Add Project

---
+++ Assets/Ghost/Scripts/GhostCatcher.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostCatcher : MonoBehaviour {
+
+	bool isCatched = false;
+	float timeSinceCatched = 0;
+	Material originalMaterial;
+	Material redMaterial;
+	MeshRenderer meshRenderer;
+
+	GameObject baseObject;
+	string obj_name;
+	// Use this for initialization
+	void Start () {
+
+		obj_name 			= this.gameObject.name + "Base";
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (isCatched) {
+			Transform transform = gameObject.transform;
+			transform.localScale *= 1f - Time.deltaTime * 0.4f;
+			transform.Rotate (Vector3.forward * timeSinceCatched * 5f);
+
+			timeSinceCatched += Time.deltaTime;
+
+			if (timeSinceCatched > 5) {
+				Destroy (gameObject);
+			}
+		}
+
+	}
+
+	void OnMouseDown(){
+
+		Debug.Log ("OMD " + obj_name);
+		isCatched = true;
+	}
+}
---
+++ Assets/Ghost/Scripts/OnTouchDown.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OnTouchDown : MonoBehaviour
+{
+	void Update () {
+
+		RaycastHit hit = new RaycastHit();
+		for (int i = 0; i < Input.touchCount; ++i) {
+			Debug.Log (Input.GetTouch (i));
+			Debug.Log ("touched");
+			if (Input.GetTouch(i).phase.Equals(TouchPhase.Began)) {
+				// Construct a ray from the current touch coordinates
+				Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+				if (Physics.Raycast(ray, out hit)) {
+					hit.transform.gameObject.SendMessage("OnMouseDown");
+				}
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			Debug.Log (ray.ToString ());
+			if (Physics.Raycast(ray, out hit)) {
+				hit.transform.gameObject.SendMessage("OnMouseDown");
+			}
+
+		}
+	}
+}
