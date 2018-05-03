7fb7aac627b6156c3f6ad56d887985f48cf7d004
Lucas <lucaskvn@gmail.com>
2018-05-03 09:24:59 +0200

    Added start menu, more ghosts, new image target

---
+++ Assets/Scripts/LoadSceneByIndex.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneByIndex : MonoBehaviour {
+
+	public void loadSceneByIndex(int sceneIndex)
+	{
+		SceneManager.LoadScene (sceneIndex);
+	}
+}
