ec4a9f10b3b13c944c2651bc1e9fdb4d7517800e
Lucas <lucaskvn@gmail.com>
2018-10-23 14:35:07 +0200

    updated dialogue creation by adding the methods from dialogue.cs

--- Assets/Scripts/Custom/ResetLeaderboard.cs
+++ Assets/Scripts/Custom/ResetLeaderboard.cs
@@ -1,34 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ResetLeaderboard : MonoBehaviour {
+public class ResetLeaderboard : MonoBehaviour
+{
 
-    private Rect windowRect;
-    private GUIStyle headerStyle;
-    private GUIStyle labelStyle;
-    private GUIStyle btnStyle;
-    private GUIStyle titleStyle;
     private bool show;
+    private Dialogue dialogue;
 
     // Use this for initialization
-    void Start () {
-
+    void Start()
+    {
         show = false;
-
-        windowRect = new Rect(
-            (Screen.width - (Screen.width / 10 * 8)) / 2,
-            (Screen.height - (Screen.height / 10 * 3)) / 2,
-            (Screen.width / 10 * 8),
-            (Screen.height / 100 * 20)
-        );
-
+        dialogue = new Dialogue("Scoreboard zurücksetzen", "\nWillst du wirklich\ndas Scoreboard zurücksetzen?", "Akzeptieren", "Abbrechen");
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!string.IsNullOrEmpty(dialogue.Decision))
+        {
+            if (dialogue.Decision == "true")
+            {
+                LeaderboardReset();
+                dialogue.Decision = "";
+            }
+            else
+            {
+                show = false;
+                dialogue.Decision = "";
+            }
+        }
+    }
 
     public void OnClick()
     {
@@ -37,79 +38,22 @@ public class ResetLeaderboard : MonoBehaviour {
 
     private void OnGUI()
     {
-        headerStyle = new GUIStyle("window");
-        labelStyle = new GUIStyle("label");
-        btnStyle = new GUIStyle("button");
-        titleStyle = new GUIStyle("label");
-
-        headerStyle.border = new RectOffset(10, 10, 10, 10);
-
-        titleStyle.fontSize = 60;
-        titleStyle.alignment = TextAnchor.MiddleCenter;
-
-        labelStyle.fontSize = 50;
-        labelStyle.alignment = TextAnchor.MiddleCenter;
-
-        btnStyle.fontSize = 40;
-
         if (show)
         {
-            windowRect = GUI.Window(0, windowRect, DialogWindow, "", headerStyle);
+            Rect windowRect = GUI.Window(0, dialogue.WindowRect, dialogue.CreateDialogue, "", dialogue.HeaderStyle);
         }
     }
 
-    // This is the actual window.
-    private void DialogWindow(int windowID)
+    private void LeaderboardReset()
     {
-        float y = windowRect.height / 2;
+        PlayerPrefs.DeleteAll();
+        Leaderboard[] highscores = GameObject.Find("Canvas").GetComponents<Leaderboard>();
 
-        Rect titleRect = new Rect(
-            5,
-            5,
-            windowRect.width,
-            windowRect.height / 100 * 20
-        );
-
-        Rect questionRectangle = new Rect(
-            5,
-            5,
-            windowRect.width,
-            windowRect.height / 100 * 70
-        );
-
-        GUI.Label(titleRect, "Scoreboard zurücksetzen", titleStyle);
-        GUI.Label(questionRectangle, "\n\nWillst du wirklich\ndas Scoreboard zurücksetzen?.", labelStyle);
-
-        Rect yesButton = new Rect(
-            windowRect.width / 2 + 10,
-            windowRect.height / 100 * 70,
-            windowRect.width / 2 - 20,
-            (windowRect.height - (y + 2)) / 2
-        );
-
-        if (GUI.Button(yesButton, "Akzeptieren", btnStyle))
+        foreach (Leaderboard highscore in highscores)
         {
-            PlayerPrefs.DeleteAll();
-            Leaderboard[] highscores = GameObject.Find("Canvas").GetComponents<Leaderboard>();
-
-            foreach (Leaderboard highscore in highscores)
-            {
-                highscore.UpdateLeaderboardText();
-            }
-
-            show = false;
+            highscore.UpdateLeaderboardText();
         }
 
-        Rect noButton = new Rect(
-            10,
-            windowRect.height / 100 * 70,
-            windowRect.width / 2 - 20,
-            (windowRect.height - (y + 2)) / 2
-        );
-
-        if (GUI.Button(noButton, "Abbrechen", btnStyle))
-        {
-            show = false;
-        }
+        show = false;
     }
 }
