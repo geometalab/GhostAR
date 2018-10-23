098e80e1f55ef03de8ec303ec7571ee826d43975
Lucas <lucaskvn@gmail.com>
2018-10-23 16:16:25 +0200

    updated dialogue creation by adding the methods from dialogue.cs
    for powerup.cs

--- Assets/Scripts/Custom/PowerUp.cs
+++ Assets/Scripts/Custom/PowerUp.cs
@@ -11,10 +11,7 @@ public class PowerUp : MonoBehaviour
     private Text PowerUpNo;
     private GhostCatcher[] ghostCatchers;
     private Rect windowRect;
-    private GUIStyle headerStyle;
-    private GUIStyle labelStyle;
-    private GUIStyle btnStyle;
-    private GUIStyle titleStyle;
+    private Dialogue dialogue;
 
     private void Start()
     {
@@ -23,18 +20,18 @@ public class PowerUp : MonoBehaviour
         show = false;
         PowerUpNo = GetComponent<Score>().PowerUpNotAvailable;
         ghostCatchers = FindObjectsOfType<GhostCatcher>();
-        windowRect = new Rect(
-            (Screen.width - (Screen.width / 10 * 8)) / 2,
-            (Screen.height - (Screen.height / 10 * 3)) / 2,
-            (Screen.width / 10 * 8),
-            (Screen.height / 100 * 20)
-        );
     }
 
     public void OnClick(GhostCatcher ghost)
     {
         if (usages < 3)
         {
+            dialogue = new Dialogue(
+                "Nutzung: " + usages + "/3",
+                "\n\nWenn du diesen Geist fängst \r\nwirst du nur die halben Punkte bekommen.",
+                "Akzeptieren",
+                "Abbrechen"
+            );
             show = true;
             this.ghost = ghost;
         }
@@ -59,91 +56,51 @@ public class PowerUp : MonoBehaviour
                 }
             }
         }
+
+        if(dialogue != null)
+        {
+            if (!string.IsNullOrEmpty(dialogue.Decision))
+            {
+                if (dialogue.Decision == "true")
+                {
+                    ResetGhostColor();
+                    dialogue.Decision = "";
+                }
+                else
+                {
+                    show = false;
+                    dialogue.Decision = "";
+                }
+            }
+        }
     }
 
     private void OnGUI()
     {
-        headerStyle = new GUIStyle("window");
-        labelStyle = new GUIStyle("label");
-        btnStyle = new GUIStyle("button");
-        titleStyle = new GUIStyle("label");
-
-        headerStyle.border = new RectOffset(10, 10, 10, 10);
-
-        titleStyle.fontSize = 60;
-        titleStyle.alignment = TextAnchor.MiddleCenter;
-
-        labelStyle.fontSize = 50;
-        labelStyle.alignment = TextAnchor.MiddleCenter;
-
-        btnStyle.fontSize = 40;
-
         if (show)
         {
-            windowRect = GUI.Window(0, windowRect, DialogWindow, "", headerStyle);
+            windowRect = GUI.Window(0, dialogue.WindowRect, dialogue.CreateDialogue, "", dialogue.HeaderStyle);
         }
     }
-
-    // This is the actual window.
-    private void DialogWindow(int windowID)
+    
+    private void ResetGhostColor()
     {
-        float y = windowRect.height / 2;
-
-        Rect titleRect = new Rect(
-            5,
-            5,
-            windowRect.width,
-            windowRect.height / 100 * 20
-        );
-
-        Rect questionRectangle = new Rect(
-            5,
-            5,
-            windowRect.width,
-            windowRect.height / 100 * 70
-        );
-
-        GUI.Label(titleRect, "Nutzung: " + usages + "/3", titleStyle);
-        GUI.Label(questionRectangle, "\n\nWenn du diesen Geist fängst \r\nwirst du nur die halben Punkte bekommen.", labelStyle);
-
-        Rect yesButton = new Rect(
-            windowRect.width / 2 + 10,
-            windowRect.height / 100 * 70,
-            windowRect.width / 2 - 20,
-            (windowRect.height - (y + 2)) / 2
-        );
-
-        if (GUI.Button(yesButton, "Akzeptieren", btnStyle))
+        usages++;
+        foreach (GhostCatcher ghost in ghostCatchers)
         {
-            usages++;
-            foreach (GhostCatcher ghost in ghostCatchers)
-            {
-                ghost.ColorOfLastCaughtGhost = " ";
-            }
-            GetComponent<Score>().SetGhostColorText("-");
-            GetComponent<Score>().DecreaseScore(50);
-            show = false;
-            GetComponent<EndScreen>().SetEndScreenInfo();
-            if (ghost)
-            {
-                ghost.CatchGhost();
-            }
-            else
-            {
-                Debug.LogError("Couldn't find the ghost to catch");
-            }
+            ghost.ColorOfLastCaughtGhost = " ";
         }
-
-        Rect noButton = new Rect(
-            10,
-            windowRect.height / 100 * 70,
-            windowRect.width / 2 - 20,
-            (windowRect.height - (y + 2)) / 2
-        );
-
-        if (GUI.Button(noButton, "Abbrechen", btnStyle))
+        GetComponent<Score>().SetGhostColorText("-");
+        GetComponent<Score>().DecreaseScore(50);
+        show = false;
+        GetComponent<EndScreen>().SetEndScreenInfo();
+        if (ghost)
         {
-            show = false;
+            ghost.CatchGhost();
+        }
+        else
+        {
+            Debug.LogError("Couldn't find the ghost to catch");
         }
     }
 
