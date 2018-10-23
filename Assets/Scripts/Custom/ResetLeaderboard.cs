fca211095dfae4c1332501b20c73915a2a05063c
Lucas <lucaskvn@gmail.com>
2018-10-23 10:45:52 +0200

    added script for the reset scoreboard button

---
+++ Assets/Scripts/Custom/ResetLeaderboard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetLeaderboard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClick()
+    {
+        PlayerPrefs.DeleteAll();
+        Leaderboard[] highscores = GameObject.Find("Canvas").GetComponents<Leaderboard>();
+
+        Debug.Log(highscores.Length);
+
+        foreach(Leaderboard highscore in highscores)
+        {
+            highscore.UpdateLeaderboardText();
+        }
+    }
+}
